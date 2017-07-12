bebf0f292e167dfcb2ef411ba6a707d8a0f35947
lnapierk <lnapierk@nd.edu>
2017-07-12 14:44:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # create and own the directories to store results locally
 save_dir='/var/lib/tf_seq2seq_chatbot'
-sudo mkdir -p $save_dir'/data/'
-sudo mkdir -p $save_dir'/nn_models/'
-sudo mkdir -p $save_dir'/results/'
-sudo chown -R "$USER" $save_dir
+mkdir -p $save_dir'/data/'
+mkdir -p $save_dir'/nn_models/'
+mkdir -p $save_dir'/results/'
+chown -R "$USER" $save_dir
 
 # copy train and test data with proper naming
 data_dir='tf_seq2seq_chatbot/data/train'
